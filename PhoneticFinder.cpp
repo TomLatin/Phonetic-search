73fb6e9556f417bea4fd901fde7d7708f7c9396f
TomLatin <tomlatin52@gmail.com>
2020-03-22 21:41:39 +0200

    add some change in exception in demo class

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -4,8 +4,7 @@ namespace phonetic {
     std::string find(std::string text, std::string word) {
         if (word == "happ")
         {
-            std::string t=" is not a full word in the sentence";
-            throw std::out_of_range {word+" is not a full word in the sentence"};
+            throw std::runtime_error {word+" is not a full word in the sentence"};
         }
 
         return "true"; };
